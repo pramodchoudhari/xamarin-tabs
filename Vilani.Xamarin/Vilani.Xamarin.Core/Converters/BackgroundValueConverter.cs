0205198067c7f1a23509d2c80883fc8ab1621f98
ADMIN <ADMIN@DESKTOP-9DJ0SMJ>
2017-07-29 16:38:37 +0530

    Color Converter

--- Vilani.Xamarin/Vilani.Xamarin.Core/Converters/BackgroundValueConverter.cs
+++ Vilani.Xamarin/Vilani.Xamarin.Core/Converters/BackgroundValueConverter.cs
@@ -1,73 +1,61 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Globalization;
-//using System.Linq;
-//using System.Text;
+using System;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
 
-//using Android.App;
-//using Android.Content;
-//using Android.OS;
-//using Android.Runtime;
-//using Android.Views;
-//using Android.Widget;
+using MvvmCross.Plugins.Color;
+using MvvmCross.Platform.UI;
 
-//using MvvmCross.Platform.Converters;
-//using Android.Graphics.Drawables;
-//using Android.Graphics;
-//using MvvmCross.Plugins.Color;
-//using MvvmCross.Platform.UI;
+namespace Vilani.Xamarin.Core.Converters
+{
+    public class TextColorValueConverter : MvxColorValueConverter
+    {
+        private static readonly MvxColor ErrorTextColor = new MvxColor(0xFF, 0x00, 0x00);
+        private static readonly MvxColor StandardTextColor = new MvxColor(0x0, 0x0, 0x0);
 
-//namespace Vilani.Xamarin.Core.Converters
-//{
-//    public class TextColorValueConverter : MvxColorValueConverter
-//    {
-//        private static readonly MvxColor ErrorTextColor = new MvxColor(0xFF, 0x00, 0x00);
-//        private static readonly MvxColor StandardTextColor = new MvxColor(0x0, 0x0, 0x0);
+        protected override MvxColor Convert(object value, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? ErrorTextColor : StandardTextColor;
+        }
+    }
 
-//        protected override MvxColor Convert(object value, object parameter, System.Globalization.CultureInfo culture)
-//        {
-//            return (bool)value ? ErrorTextColor : StandardTextColor;
-//        }
-//    }
+    public class StringLengthValueConverter : MvxValueConverter<string>
+    {
+        protected override object Convert(string value, Type targetType, object parameter, CultureInfo culture)
+        {
+            value = value ?? string.Empty;
+            return value.Length;
+        }
+    }
+    public class BackgroundColorValueConverter : MvxValueConverter<bool>
+    {
+        protected override object Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ? new MvxColor(0xFF, 0x00, 0x00) : new MvxColor(0x0, 0x0, 0x0);
+        }
+    }
 
-//    public class StringLengthValueConverter : MvxValueConverter<string>
-//    {
-//        protected override object Convert(string value, Type targetType, object parameter, CultureInfo culture)
-//        {
-//            value = value ?? string.Empty;
-//            return value.Length;
-//        }
-//    }
-//    public class BackgroundColorValueConverter : MvxValueConverter<bool>
-//    {
-//        protected override object Convert(bool value, Type targetType, object parameter, CultureInfo culture)
-//        {
-//            return value ? new ColorDrawable(new Color(255, 255, 255)) : new ColorDrawable(new Color(255, 0, 0));
-//        }
-//    }
+    //public class BackgroundValueConverter : MvxValueConverter<bool, MvxColor>
+    //{
+    //    protected override MvxColor Convert(bool value, System.Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        return value ? new MvxColor(0xFF, 0x00, 0x00) : new MvxColor(0x0, 0x0, 0x0); ;
+    //    }
+    //}
 
-//    public class BackgroundValueConverter : MvxValueConverter<bool, ColorDrawable>
-//    {
-//        protected override ColorDrawable Convert(bool value, System.Type targetType, object parameter, CultureInfo culture)
-//        {
-//            return value ? new ColorDrawable(new Color(0, 0, 255)) : new ColorDrawable(new Color(255, 0, 0));
-//        }
-//    }
+    //public class AssetsPathConverter : IMvxValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        return value.ToString().Replace("../Assets/", string.Empty);
+    //    }
 
-//    //public class AssetsPathConverter : IMvxValueConverter
-//    //{
-//    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-//    //    {
-//    //        return value.ToString().Replace("../Assets/", string.Empty);
-//    //    }
+    //    #region Implementation of IMvxValueConverter
 
-//    //    #region Implementation of IMvxValueConverter
+    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        return value;
+    //    }
 
-//    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-//    //    {
-//    //        return value;
-//    //    }
-
-//    //    #endregion
-//    //}
-//}
+    //    #endregion
+    //}
+}
